f5e5df695b6a6b8050e9dd2ac655493b80a6da6a
Jinyan Chan <Jinyan_Chan@baylor.edu>
2020-06-03 03:10:03 +0000

    version 0.1.2

--- inst/doc/my-vignette.R
+++ inst/doc/my-vignette.R
@@ -1,10 +1,10 @@
-## ----eval=F--------------------------------------------------------------
+## ----eval=F-------------------------------------------------------------------
 #  library("DrInsight")
 #  # load in and process the downloaded CMap drug rank matrix
 #  cmap.ref.profiles = get.cmap.ref(cmap.data.path = '/path_to/rankMatrix.txt',
 #                                   probe.to.genes = probe.to.genes, drug.info = drug.info)
 
-## ----eval=F--------------------------------------------------------------
+## ----eval=F-------------------------------------------------------------------
 #  # select the drug p value table
 #  drug.pvals = drug.ident.res$drug.pvals
 
